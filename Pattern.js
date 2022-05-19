71960a788b7468b5b79b839678f93b13ac9b2f26
Avishek Thapa <avishekthapa1997@gmail.com>
2022-05-19 21:24:38 +0530

    add Pattern-3 logic

---
+++ Pattern.js
@@ -0,0 +1,10 @@
+const createPattern3 = (n) => {
+  for (let i = n; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      process.stdout.write("*");
+    }
+    process.stdout.write("\n");
+  }
+};
+
+createPattern3(8);
