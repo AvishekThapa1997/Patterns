d41f005841c4652713684c49e81cc0b0b8fadf42
Avishek Thapa <avishekthapa1997@gmail.com>
2022-05-23 18:10:37 +0530

    add Pattern-29 logic

--- Pattern.js
+++ Pattern.js
@@ -1,11 +1,14 @@
-const createPattern17 = (n) => {
-  for (let i = 0; i < 2 * n - 1; i++) {
-    const min = i < n ? n - i - 1 : i - n + 1;
-    const max = i < n ? i + n - 1 : 2 * n - 2 - (i - n + 1);
+const createPattern29 = (n) => {
+  for (let i = 0; i < n; i++) {
+    const min = n - i - 1;
+    const max = i + n - 1;
     let flag = false;
     let count = i < n ? i + 1 : n - min;
     for (let j = 0; j < 2 * n - 1; j++) {
       if (j >= min && j <= max) {
+        if (j > min) {
+          process.stdout.write(" ");
+        }
         process.stdout.write(count.toString());
         if (count === 1) {
           flag = true;
@@ -13,10 +16,8 @@ const createPattern17 = (n) => {
         flag ? ++count : --count;
         continue;
       }
-      process.stdout.write(" ");
+      process.stdout.write("  ");
     }
     process.stdout.write("\n");
   }
 };
-
-createPattern17(5);
